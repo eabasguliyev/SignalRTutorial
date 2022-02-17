39e752fe4ba332aa4f74e682080f6a6b2c55c542
eabasquliyev <elgun.abasquliyev2@gmail.com>
2022-02-17 17:14:33 +0400

    Connection User List

--- ServerClientConnection/ServerClient/ServerClient/Hubs/MyHub.cs
+++ ServerClientConnection/ServerClient/ServerClient/Hubs/MyHub.cs
@@ -1,11 +1,18 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ServerClient.Hubs
 {
     public class MyHub: Hub
     {
+        static List<string> ConnectedClients;
+
+        static MyHub()
+        {
+            ConnectedClients = new List<string>();
+        }
         public async Task SendMessageAsync(string message)
         {
             // some logic
@@ -18,11 +25,15 @@ namespace ServerClient.Hubs
         {
             // You can get information about client from Context property
             // Context.ConnectionId
+            ConnectedClients.Add(Context.ConnectionId);
+            await Clients.All.SendAsync("connectedClients", ConnectedClients);
             await Clients.All.SendAsync("userJoined", Context.ConnectionId);
         }
 
         public async override Task OnDisconnectedAsync(Exception exception)
         {
+            ConnectedClients.Remove(Context.ConnectionId);
+            await Clients.All.SendAsync("connectedClients", ConnectedClients);
             await Clients.All.SendAsync("userLeft", Context.ConnectionId);
         }
     }
